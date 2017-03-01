5ada6692a992d4544d01bc61b6f480a4e2a61b40
Rupert Horlick <ruhatch@me.com>
2017-03-01 14:13:17 +0000

    Sped up implementation and inlined stuff

--- Artemis.hs
+++ Artemis.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE BangPatterns #-}
 
+import Control.Monad (forM_)
 import Data.ByteString.Lazy.Char8 (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as BS
 import Data.Ord
 import Data.Map (Map)
-import qualified Data.Map.Strict as Map
+import qualified Data.Map as Map
 import Data.IntSet (IntSet)
 import qualified Data.IntSet as Set
 import Data.Vector (Vector)
@@ -18,10 +19,10 @@ main :: IO ()
 main = do
   [f] <- getArgs
   (l1 : l2 : ls) <- BS.lines <$> BS.readFile f
-  let [v , e , r , c , x] = map readInt (BS.split ' ' l1)
+  let [v , e , _ , c , x] = map readInt (BS.split ' ' l1)
   let videoSizes = readIntLine l2
   (endpoints , ls') <- readEndpoints e ls
-  requests <- readRequests v videoSizes e r ls'
+  requests <- readRequests v videoSizes e ls'
   results <- calculateCaches c x videoSizes endpoints requests
   printResults results
 
@@ -45,36 +46,30 @@ readEndpoints e ls = do
 
   where
 
-    readCaches :: [ByteString] -> IOVector (Int , Int) -> Int -> Int -> Int -> IO [ByteString]
-    readCaches (l : ls) w latency numCaches i
-      | i < numCaches = do
-        let [cacheId , latency'] = map readInt (BS.split ' ' l)
-        VM.unsafeWrite w i (cacheId , latency - latency')
-        readCaches ls w latency numCaches (i + 1)
-
-      | otherwise = return (l : ls)
-
-    readCaches [] _ _ _ _ = error "Ran out of input!"
-
     readEndpoint :: [ByteString] -> IOVector (Vector (Int , Int)) -> Int -> IO [ByteString]
-    readEndpoint (l : ls) v i
+    readEndpoint (l : rest) v i
       | i < e = do
         let [latency , numCaches] = map readInt (BS.split ' ' l)
+        let (cacheLines , rest') = splitAt numCaches rest
         w <- VM.new numCaches
-        ls' <- readCaches ls w latency numCaches 0
+        forM_ (zip [0..] cacheLines) (\ (j , l') -> do
+          let [cacheId , latency'] = map readInt (BS.split ' ' l')
+          VM.unsafeWrite w j (cacheId , latency - latency'))
         VA.sortBy (comparing (Down . snd)) w
         w' <- V.unsafeFreeze w
         VM.unsafeWrite v i w'
-        readEndpoint ls' v (i + 1)
+        readEndpoint rest' v (i + 1)
 
-      | otherwise = return (l : ls)
+      | otherwise = return (l : rest)
 
     readEndpoint [] _ _ = error "Ran out of input!"
 
-readRequests :: Int -> Vector Int -> Int -> Int -> [ByteString] -> IO (Vector (Int , Int , Int))
-readRequests noVideos videoSizes e r ls = do
+readRequests :: Int -> Vector Int -> Int -> [ByteString] -> IO (Vector (Int , Int , Int))
+readRequests noVideos videoSizes e ls = do
   v <- VM.replicate (noVideos * e)  (0 , 0 , 0)
-  readRequest ls v 0
+  forM_ ls (\ l -> do
+    let [videoID , endpointID , requestSize] = map readInt (BS.split ' ' l)
+    VM.unsafeModify v (\ (_ , _ , c) -> (videoID , endpointID , c + requestSize)) (videoID * e + endpointID))
   v' <- V.unsafeFreeze v
   let v'' = V.filter (\ (_ , _ , c) -> c /= 0) v'
   v''' <- V.unsafeThaw v''
@@ -83,27 +78,14 @@ readRequests noVideos videoSizes e r ls = do
   VA.sortBy (comparing (Down . (\ (_ , _ , c) -> c))) v'''
   V.unsafeFreeze v'''
 
-  where
-
-    readRequest :: [ByteString] -> IOVector (Int , Int , Int) -> Int -> IO ()
-    readRequest (l : ls) v i
-      | i < r = do
-        let [videoID , endpointID , requestSize] = map readInt (BS.split ' ' l)
-        VM.unsafeModify v (\ (_ , _ , c) -> (videoID , endpointID , c + requestSize)) (videoID * e + endpointID)
-        readRequest ls v (i + 1)
-
-      | otherwise = return ()
-
-    readRequest [] _ i
-      | i >= r = return ()
-      | otherwise = error "Ran out of input!"
-
 calculateCaches :: Int -> Int -> Vector Int -> Vector (Vector (Int , Int)) -> Vector (Int , Int , Int) -> IO (Map Int IntSet)
 calculateCaches c x videoSizes endpoints requests = do
   caches <- VM.replicate c x
+  videos <- VM.replicate (V.length videoSizes) Set.empty
   V.foldM (\ m (videoID , endpointID , _) -> do
             let cacheList = V.map fst $ endpoints V.! endpointID
-            if inAnySets videoID cacheList m
+            videoCaches <- VM.unsafeRead videos videoID
+            if any (`Set.member` videoCaches) cacheList
               then return m
               else do
                 cacheID <- findCache caches cacheList videoID
@@ -115,18 +97,17 @@ calculateCaches c x videoSizes endpoints requests = do
                           then return m
                           else do
                             VM.unsafeModify caches (flip (-) (videoSizes V.! videoID)) cacheID
+                            VM.unsafeModify videos (Set.insert cacheID) videoID
                             return (Map.insert cacheID (Set.insert videoID resultSet) m)
                       Nothing -> do
                         VM.unsafeModify caches (flip (-) (videoSizes V.! videoID)) cacheID
+                        VM.unsafeModify videos (Set.insert cacheID) videoID
                         return (Map.insert cacheID (Set.singleton videoID) m)
                   Nothing -> return m)
           Map.empty
           requests
 
   where
-
-    inAnySets :: (Eq k) => Int -> Vector k -> Map k IntSet -> Bool
-    inAnySets a ks = not . Map.null . Map.filterWithKey (\ k v -> k `V.elem` ks && a `Set.member` v)
 
     findCache :: IOVector Int -> Vector Int -> Int -> IO (Maybe Int)
     findCache caches cacheList videoID = findCache' 0
